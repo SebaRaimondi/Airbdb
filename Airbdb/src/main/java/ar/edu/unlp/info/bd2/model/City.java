8c6018a798dd101aad3f81c204003eeed3588837
Camila Onofri <onofricamila2@gmail.com>
2018-04-10 22:11:39 -0300

    added model property relacionado a city, bidireccional

--- Airbdb/src/main/java/ar/edu/unlp/info/bd2/model/City.java
+++ Airbdb/src/main/java/ar/edu/unlp/info/bd2/model/City.java
@@ -1,21 +1,35 @@
 package ar.edu.unlp.info.bd2.model;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
+@Entity
+@Table(name="cities")
 public class City {
-    @Column(name="name", nullable=false)
-    private String name;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="cityId")
     private Long id;
 
+    @Column(name="name", nullable=false)
+    private String name;
+
+    @OneToMany(mappedBy = "cityId")
+    private List<Property> properties;
+
+    public City(){}
+
     public City(String name) {
         this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -26,11 +40,11 @@ public class City {
         this.name = name;
     }
 
-    public Long getId() {
-        return id;
+    public List<Property> getProperties() {
+        return properties;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setProperties(List<Property> properties) {
+        this.properties = properties;
     }
 }
--- Airbdb/src/main/java/ar/edu/unlp/info/bd2/model/Property.java
+++ Airbdb/src/main/java/ar/edu/unlp/info/bd2/model/Property.java
@@ -22,5 +22,63 @@ public class Property {
     @Column(nullable=false)
     private int capacity;
 
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn()
+    private City cityId;
 
+    public Property(String name, String description, double price, int capacity, City cityId) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.capacity = capacity;
+        this.cityId = cityId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public City getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(City cityId) {
+        this.cityId = cityId;
+    }
 }
