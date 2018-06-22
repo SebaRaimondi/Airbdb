07c86f045cdf2622a0f9cdca5698456bf977bcbf
Onofri Camila <onofricamila2@gmail.com>
2018-06-21 21:32:24 -0300

    database script improved. User cretion and permissions added.

--- Airbdb/createDatabase.sh
+++ Airbdb/createDatabase.sh
@@ -1,1 +1,6 @@
-mysql -u root -e "drop database if exists bd2_grupo2; create database bd2_grupo2;"
+mysql -u root -e "drop database if exists bd2_grupo2;
+                  create database bd2_grupo2;
+                  CREATE USER IF NOT EXISTS bd2_grupo2 IDENTIFIED BY 'topsecret';
+                  GRANT ALL PRIVILEGES ON bd2_grupo2.* TO 'bd2_grupo2'@'localhost';
+                  FLUSH PRIVILEGES;
+                  "
