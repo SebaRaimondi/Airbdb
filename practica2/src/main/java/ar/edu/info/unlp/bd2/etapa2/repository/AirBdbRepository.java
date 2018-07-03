41ae04e87c561c5074d63d4bf88f63e3855bf699
Onofri Camila <onofricamila2@gmail.com>
2018-07-03 20:28:21 -0300

    mongotemplate autowired en repository gral, db number set to 2 en config

--- practica2/src/main/java/ar/edu/info/unlp/bd2/etapa2/repository/AirBdbRepository.java
+++ practica2/src/main/java/ar/edu/info/unlp/bd2/etapa2/repository/AirBdbRepository.java
@@ -1,9 +1,11 @@
 package ar.edu.info.unlp.bd2.etapa2.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 public class AirBdbRepository {
 
     @Autowired
+    MongoTemplate mongoTemplate;
 
 }
