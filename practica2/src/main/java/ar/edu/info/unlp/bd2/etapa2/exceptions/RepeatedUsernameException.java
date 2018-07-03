3293dce6e1ee014d253f4ea6be0ae980d72858da
Onofri Camila <onofricamila2@gmail.com>
2018-07-03 20:31:15 -0300

    exceptions folder y  RepeatedUsernameException

---
+++ practica2/src/main/java/ar/edu/info/unlp/bd2/etapa2/exceptions/RepeatedUsernameException.java
@@ -0,0 +1,5 @@
+package ar.edu.info.unlp.bd2.etapa2.exceptions;
+
+public class RepeatedUsernameException {
+    public RepeatedUsernameException() { super("There is an existing user with that username !!!"); }
+}
